19257c13bd98cc89e1a9d2a8ef846eea48f3e6c3
MrPen <slgdy94@gmail.com>
2019-03-10 10:45:51 +0100

    zadania z lab1

---
+++ lab1_zad6.py
@@ -0,0 +1,13 @@
+# -*-utf-8 -*-
+
+import random
+import matplotlib as mpl
+import numpy as np
+import matplotlib.pylab as plt
+
+vector = []
+
+for i in range(0, 100):
+    number = random.randrange(20)
+    vector.append(number)
+
